7259d50e036adc3fa508deb1727ca00653764098
a.simakov <a.simakov@bag24.ru>
2023-04-22 11:28:57 +0300

    fix: карта

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,7 +1,7 @@
-import { View, Image, StyleSheet, TouchableOpacity, SafeAreaView, Platform, Alert, StatusBar as RNStatusBar } from 'react-native';
+import { View, Image, StyleSheet, TouchableOpacity, SafeAreaView, Platform, Alert, StatusBar as RNStatusBar, ActivityIndicator, Text } from 'react-native';
 import { DrawerActions, useFocusEffect } from '@react-navigation/native';
 import React, { useCallback, useEffect, useLayoutEffect, useState, useRef } from 'react';
-import { YaMap, Animation, Marker, Polyline } from 'react-native-yamap';
+import { YaMap, Animation, Marker, Polyline, ClusteredYamap } from 'react-native-yamap';
 import { Dimensions } from 'react-native'
 import * as Location from 'expo-location';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -25,7 +25,7 @@ const BACKGROUND_NOTIFICATION_TASK = "BACKGROUND-NOTIFICATION-TASK";
 TaskManager.defineTask(
   BACKGROUND_NOTIFICATION_TASK,
   ({ data, error, executionInfo }) => {
-    console.log(data);
+    // console.log(data);
     if (error) {
       console.log("error occurred");
     }
@@ -40,7 +40,7 @@ function MapScreen({ navigation }) {
   const [washes, setWashes] = useState({});
   const [route, setRoute] = useState([]);
   const [washeses, setWasheses] = useState([]);
-  // const [bLocation, setBLocation] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [isOpen, setDrawer] = useState(false);
   const [notification, setNotification] = useState(false);
   const notificationListener = useRef();
@@ -65,62 +65,70 @@ function MapScreen({ navigation }) {
           Alert.alert("Ошибка", "Необходимо включить определение геопозиции");
         }
         const phone = await AsyncStorage.getItem("phone"); // получение телефона из хранилища
-        let location = await AsyncStorage.getItem("location"); // получение города из хранилища
-        if (location == null) {
-          location = "Краснодар";
-        }
-        const ret = await axios.get(domain_web + "/get_all_washes", { params: { location: location, phone: phone } }); // запрос всех моек в городе
-        setWasheses(ret.data); // сохранение всех моек в State
         const res = await axios.get(domain_web + "/get_address_moika", { params: { phone: phone } }); // запрос на получение адреса мойки ближайшего заказа
         setWashes(res.data); // сохранеине адреса в State
-
         if (loc == null) { // если нет прав или не получилось получить координаты
           return;
         }
+
         // console.warn(loc.coords.latitude);
         map.current.setCenter({ lon: loc.coords.longitude, lat: loc.coords.latitude }, 15, 0, 0, 1, Animation.SMOOTH); //координаты, зум, поворотом по азимуту и наклоном карты, длительность анимации, анимация
       }
 
     })();
+    setLoading(true);
+    AsyncStorage.getItem("location").then(loc => {
+      if (loc == null) {
+        loc = "Краснодар";
+      }
+      AsyncStorage.getItem("phone").then(phone => {
+        axios.get(domain_web + "/get_all_washes", { params: { location: loc, phone: phone } })
+          .then(ret => {
+            setWasheses(ret.data);
+            setLoading(false);
+            // сохранение всех моек в State // запрос всех моек в городе
+          })
+      })
+    })
   }, []));
 
   useEffect(() => {
     registerForPushNotificationsAsync();
 
     notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-      console.log(notification);
+      // console.log(notification);
       setNotification(notification);
     });
 
     responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log(response);
-      if (Platform.OS == "ios"){
-        if (response.notification.request.content.categoryIdentifier == "successful"){
+      // console.log(response);
+      if (Platform.OS == "ios") {
+        if (response.notification.request.content.categoryIdentifier == "successful") {
           navigation.navigate("PersonalAccount");
           navigation.navigate("MyOrders");
-          navigation.navigate('OrderDetails', {orderId: response.notification.request.trigger.payload.order});
-          navigation.navigate('EvaluateService', {orderId: response.notification.request.trigger.payload.order});
+          navigation.navigate('OrderDetails', { orderId: response.notification.request.trigger.payload.order });
+          navigation.navigate('EvaluateService', { orderId: response.notification.request.trigger.payload.order });
           return;
         }
-        else if(response.notification.request.content.categoryIdentifier == "new_stock"){
+        else if (response.notification.request.content.categoryIdentifier == "new_stock") {
           navigation.navigate("Catalog");
           return;
         }
       }
-      else if (Platform.OS == "android"){
-        if (response.notification.request.trigger.remoteMessage.notification.tag == "successful"){
+      else if (Platform.OS == "android") {
+        if (response.notification.request.trigger.remoteMessage.notification.tag == "successful") {
           navigation.navigate("PersonalAccount");
           navigation.navigate("MyOrders");
-          navigation.navigate('OrderDetails', {orderId: response.notification.request.trigger.remoteMessage.data.order});
-          navigation.navigate('EvaluateService', {orderId: response.notification.request.trigger.remoteMessage.data.order});
+          navigation.navigate('OrderDetails', { orderId: response.notification.request.trigger.remoteMessage.data.order });
+          navigation.navigate('EvaluateService', { orderId: response.notification.request.trigger.remoteMessage.data.order });
           return;
         }
-        else if(response.notification.request.trigger.remoteMessage.notification.tag == "new_stock"){
+        else if (response.notification.request.trigger.remoteMessage.notification.tag == "new_stock") {
           navigation.navigate("Catalog");
           return;
         }
       }
-      
+
     });
 
     return () => {
@@ -133,21 +141,21 @@ function MapScreen({ navigation }) {
     // const pushtoken = await AsyncStorage.getItem("pushToken");
     if (Device.isDevice) {
       const devicePushToken = (await Notifications.getDevicePushTokenAsync()).data
-      console.log(devicePushToken);
+      // console.log(devicePushToken);
       // if (pushtoken == null) {
-        try{
-          const token = await AsyncStorage.getItem("token");
-          const device = Platform.OS == "ios";
-          if (token != null) {
-            await axios.post(domain_mobile + "/api/set_push_token", { token: devicePushToken, device: device }, { headers: { "Authorization": "Token " + token } });
-          }else{
-            await axios.post(domain_mobile + "/api/set_push_token", { token: devicePushToken, device: device });
-          }
-          await AsyncStorage.setItem("pushToken", devicePushToken);
-        }catch(err){
-          console.log(err);
+      try {
+        const token = await AsyncStorage.getItem("token");
+        const device = Platform.OS == "ios";
+        if (token != null) {
+          await axios.post(domain_mobile + "/api/set_push_token", { token: devicePushToken, device: device }, { headers: { "Authorization": "Token " + token } });
+        } else {
+          await axios.post(domain_mobile + "/api/set_push_token", { token: devicePushToken, device: device });
         }
-        
+        await AsyncStorage.setItem("pushToken", devicePushToken);
+      } catch (err) {
+        console.log(err);
+      }
+
       // }
 
       const { status: existingStatus } = await Notifications.getPermissionsAsync();
@@ -187,8 +195,9 @@ function MapScreen({ navigation }) {
   }
   zoomUp = async () => { // приближение
     const position = await getCurrentPosition();
+    // console.log(position);
     if (map.current) {
-      // console.warn(Object.keys(map.current.props));
+      // console.warn(map.current.props['children'][1]);
       map.current.setZoom(position.zoom * 1.1, 0.5);
     }
   };
@@ -250,29 +259,61 @@ function MapScreen({ navigation }) {
       return;
     }
   }
+
+  // if (loading) {
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
+  //       <ActivityIndicator color='black' size={'large'} />
+  //     </View>
+  //   )
+  // }
+
   //ПЕРЕСМОТРЕТЬ КАРТУУУУ!!!!!
   // YaMap.resetLocale(); // язык системы
   return (
     <View style={styles.container}>
       <StatusBar />
-      <YaMap
+      {loading &&
+        <View style={{ justifyContent: 'center', alignItems: 'center', position: 'absolute', width: '100%', height: '100%', zIndex: 1 }} >
+          <ActivityIndicator color='black' size={'large'} />
+          <Text>Идет загрузка карты</Text>
+        </View>}
+      <ClusteredYamap
         ref={map}
         // userLocationIcon={{ uri: 'https://www.clipartmax.com/png/middle/180-1801760_pin-png.png' }}
         style={styles.container}
+        clusterColor="blue"
+        clusteredMarkers={
+          washeses.map(obj => {
+            return ({
+              point: {
+                lat: parseFloat(obj.lat),
+                lon: parseFloat(obj.lon),
+              },
+              data: {
+                avatar: obj.avatar,
+                id: obj.id,
+                sale: obj.sale,
+              }
+            })
+          })
+        }
+        renderMarker={(info, index) => (
+          <Marker scale={0.6} key={index} point={info.point}
+            source={require('../assets/images/location.png')} // {{ uri: domain_web + obj.avatar }}
+            onPress={() => {
+              (async () => {
+                await AsyncStorage.setItem("washer", info.data.id.toString());
+                await AsyncStorage.setItem("sale", info.data.sale.toString());
+                navigation.navigate('Catalog');
+                navigation.navigate('PointCarWash');
+              })();
+            }}
+          />
+        )}
       >
-        {washeses.map(obj => <Marker scale={0.05} key={obj.id} point={{ lat: parseFloat(obj.lat), lon: parseFloat(obj.lon) }}
-          source={{ uri: domain_web + obj.avatar }}
-          onPress={() => {
-            (async () => {
-              await AsyncStorage.setItem("washer", obj.id.toString());
-              await AsyncStorage.setItem("sale", obj.sale.toString());
-              navigation.navigate('Catalog')
-              navigation.navigate('PointCarWash')
-            })();
-          }}
-        />)}
         {route != [] && <Polyline strokeWidth={7} strokeColor="#7CD0FF" points={route} />}
-      </YaMap>
+      </ClusteredYamap>
 
       {/* Dimensions.get('window').width */}
       {!isOpen &&
