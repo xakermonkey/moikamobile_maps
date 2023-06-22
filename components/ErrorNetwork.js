2d43859db5b85391a0af2aedcff1275dffa0d74b
blukky <ya.gud@mail.ru>
2023-06-22 16:09:37 +0300

    fix(car washer): add handler internet error

---
+++ components/ErrorNetwork.js
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, View, Image, ImageBackground, StatusBar, TouchableOpacity } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import NetInfo from "@react-native-community/netinfo";
+
+
+const ErrorNetwork = ({reconnectServer, title}) => {
+
+    const [showElement, setShowElement] = useState(false);
+
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+          setShowElement(true);
+        }, 5000);
+    
+        return () => clearTimeout(timeout); // Очистка таймера при размонтировании компонента
+      }, []);
+
+    return (
+        <View style={styles.container}>
+            <StatusBar />
+            <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1, padding: '5%' }}>
+                <Image source={require('../assets/images/logo_succes.png')} />
+                <Text style={[styles.bold_text, { textAlign: 'center' }]}>{title}</Text>
+                <View style={{ width: '100%', marginTop: '20%' }}>
+                    {showElement && <TouchableOpacity activeOpacity={0.8} onPress={reconnectServer} style={{}} >
+                        <ImageBackground source={require('../assets/images/button.png')} resizeMode='stretch' style={styles.bg_img} >
+                            <Text style={styles.text_btn} >Повторить</Text>
+                        </ImageBackground>
+                    </TouchableOpacity>}
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default ErrorNetwork
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#6E7476',
+        flex: 1,
+    },
+    bold_text: {
+        marginTop: '5%',
+        fontSize: 18,
+        color: '#fff',
+        fontFamily: 'Raleway_700Bold',
+        textTransform: 'uppercase',
+    },
+    text_btn: {
+        fontSize: 14,
+        color: '#FFFFFF',
+        fontFamily: 'Raleway_400Regular',
+        textTransform: 'uppercase',
+        paddingVertical: '5%',
+    },
+    bg_img: {
+        alignItems: 'center',
+        height: 52
+    },
+})
